183cc4201733f8b8bff75e58ee7b30f2c6d017f5
MayuriKolhe-2003 <kolhem10@gmail.com>
2024-07-02 21:23:27 +0530

    Add (Blogs)

---
+++ src/JSON/BlogData.js
@@ -0,0 +1,37 @@
+const BlogData = [
+    
+    {
+      "title": "The Future of Technology",
+      "blogImg": "https://media.licdn.com/dms/image/D4D22AQEmRAuxTF6yCA/feedshare-shrink_800/0/1719489201026?e=1722470400&v=beta&t=UWccDTjQJV9e8D77192kTS4mtpi2rH5yEe5zxoaynVo",
+      "username": "Jane Doe",
+      "userImg": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-55958-614810.jpg&fm=jpg",
+      "publishDate": "2024-06-29",
+      "category": "AI"
+    },
+    {
+      "title": "Healthy Eating: Tips and Tricks",
+      "blogImg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRirz5J2Vek3gao1dbFyU0XNOqVr26_1xlqA&s",
+      "username": "John Smith",
+      "userImg": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-55958-614810.jpg&fm=jpg",
+      "publishDate": "2024-06-25",
+      "category": "ML"
+    },
+    {
+      "title": "Exploring the World: Top Travel Destinations",
+      "blogImg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUr7eiXkCCekoCDOFee8n1vt21DJ0sK0BBBQ&s",
+      "username": "Emily Johnson",
+      "userImg": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-55958-614810.jpg&fm=jpg",
+      "publishDate": "2024-06-20",
+      "category": "AWS"
+    },
+    {
+        "title": "Exploring the World: Top Travel Destinations",
+        "blogImg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUr7eiXkCCekoCDOFee8n1vt21DJ0sK0BBBQ&s",
+        "username": "Emily Johnson",
+        "userImg": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-55958-614810.jpg&fm=jpg",
+        "publishDate": "2024-06-20",
+        "category": "Web"
+      }
+]
+
+export default BlogData;
--- src/components/HomePage/Blogs.jsx
+++ src/components/HomePage/Blogs.jsx
@@ -1,15 +1,99 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../util/Blogs.css";
+import BlogData from "../../JSON/BlogData";
 
 const BlogsPage = () => {
-
+    // Extracting unique categories from BlogData
+    const uniqueCategories = [...new Set(BlogData.map(blog => blog.category))];
+  
+    // State to track selected category
+    const [selectedCategory, setSelectedCategory] = useState(uniqueCategories[0]); // Initialize with the first category
+  
+    // Filter blogs based on selected category
+    const filteredBlogs = BlogData.filter(blog => blog.category === selectedCategory);
+  
+    // Function to handle category selection
+    const handleCategoryChange = (category) => {
+      setSelectedCategory(category);
+    };
+  
     return (
-
-        <div class=" h-screen w-screen bg-gradient-to-b from-back-dark to-back-light">
-            <h1 className="text-white">Blogs Page</h1>
+      <div className="h-screen w-screen bg-gradient-to-b from-back-dark to-back-light">
+        <h1 className="text-6xl font-bold text-center text-white">
+          Our{" "}
+          <span className="bg-gradient-to-r from-orange-500 via-indigo-500 to-green-500 text-transparent bg-clip-text">
+            Blogs
+          </span>
+        </h1>
+  
+        <nav className="bg-white shadow dark:bg-gray-800 mt-5">
+          <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            {uniqueCategories.map((category, index) => (
+              <button
+                key={index}
+                onClick={() => handleCategoryChange(category)}
+                className={`border-b-2 border-transparent mx-1.5 sm:mx-6 ${
+                  selectedCategory === category
+                    ? "text-gray-800 dark:text-gray-200 border-blue-500"
+                    : "hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500"
+                }`}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+        </nav>
+  
+        <div className="card-container w-5/6 mx-auto my-7 h-screen flex flex-wrap justify-center items-center transition-all duration-200 ease-in-out">
+          {filteredBlogs.length === 0 ? (
+            <p className="text-gray-600 text-center">No blogs found for selected category.</p>
+          ) : (
+            filteredBlogs.map((blog, index) => (
+              <div className="card shadow-lg" key={index}>
+                <div
+                  className="card-img bg-no-repeat bg-center bg-cover"
+                  style={{ backgroundImage: `url(${blog.blogImg})` }}
+                ></div>
+                <a href="#" className="card-link">
+                  <div
+                    className="card-img-hovered"
+                    style={{ backgroundImage: `url(${blog.blogImg})` }}
+                  ></div>
+                </a>
+  
+                <div className="card-info">
+                  <div className="card-about">
+                    <a className="card-tag tag-1">{blog.category}</a>
+                    <div className="card-time">{blog.publishDate}</div>
+                  </div>
+                  <h1 className="card-title">{blog.title}</h1>
+                </div>
+  
+                <div className="flex items-center px-6 pb-4">
+                  <a href="#">
+                    <img
+                      className="w-12 h-12 rounded-full mr-2"
+                      src={blog.userImg}
+                      alt={blog.username}
+                    />
+                  </a>
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="text-gray-900 font-medium leading-none hover:text-indigo-600"
+                    >
+                      {blog.username}
+                    </a>
+                    <p className="text-gray-600">{blog.publishDate}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
         </div>
+      </div>
     );
-
-};
-
-export default BlogsPage;
+  };
+  
+  export default BlogsPage;
